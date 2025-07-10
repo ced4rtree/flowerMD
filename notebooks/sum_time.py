9e7d11e26fa9d1eb8b21e5e81e8cbdce36b032d4
ced4rtree <usernamesarenoteasy@proton.me>
2025-07-10 15:30:45 -0600

    improved sum_time.py

--- notebooks/sum_time.py
+++ notebooks/sum_time.py
@@ -11,15 +11,20 @@ for log_dir in glob.glob("notebooks/*logs"):
             if os.path.exists(f"{log_run}/parameters.txt"):
                 with open(fr"{log_run}/parameters.txt", 'r') as parameter_file:
                     parameters = parameter_file.readlines()
-                    steps_index = 0
+                    shrink_steps_index = 0
+                    static_steps_index = 0
                     for parameter in parameters:
                         if parameter.find("shrink_steps") != -1:
-                            steps_index = parameters.index(parameter)
+                            shrink_steps_index = parameters.index(parameter)
+                        if parameter.find("static_steps") != -1:
+                            static_steps_index = parameters.index(parameter)
                         tps_arr = log["flowermdbasesimulationSimulationtps"]
                         tps_arr = tps_arr[tps_arr != 0]
+                        shrink_steps = float(parameters[shrink_steps_index].split()[-1])
+                        static_steps = float(parameters[static_steps_index].split()[-1])
+                        total_steps = shrink_steps + static_steps
                         if (len(tps_arr) != 0):
-                            steps = float(parameters[steps_index].split()[-1])
-                            runtime = (steps / len(tps_arr)) / tps_arr
+                            runtime = (total_steps / len(tps_arr)) / tps_arr
                             total_time += numpy.sum(runtime)
 
 print("Total runtime (s): ", total_time)
